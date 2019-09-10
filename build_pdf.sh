25c6c64fc19010686656929eb157a49abfe98b81
Theodore Cowan <theodore-cowan@pluralsight.com>
2019-09-10 10:43:15 -0400

    add build script for pdf

---
+++ build_pdf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -x "`type -P rst2pdf`" ]; then
+	FILENAME="Theodore_Cowan_Resume-`date +%F`.pdf"
+	echo "Creating PDF $FILENAME"
+	rst2pdf -o $FILENAME README.rst --stylesheets=stylesheet.css --real-footnotes
+	echo "Completed PDF"
+else
+	echo "Please install rst2pdf" ; exit 1
+fi
+
+
